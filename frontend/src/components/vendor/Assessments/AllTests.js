ba3c30e6153a5fbf0ca40c6fca2a225449566409
C Sarath <sarathyadav112@gmail.com>
2024-12-05 13:17:27 +0530

    updated All test

--- frontend/src/components/vendor/Assessments/AllTests.js
+++ frontend/src/components/vendor/Assessments/AllTests.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState,  useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Popover } from '@headlessui/react';
 import { toast } from 'react-hot-toast';
